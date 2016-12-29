a891fad51bd225c00ecfc6a6a2cd32654bd8b488
maderin <alperenderin@gmail.com>
2016-12-30 01:04:17 +0300

    added other things as parameter to see them at test file.

--- nlp_gensim_test.py
+++ nlp_gensim_test.py
@@ -3,18 +3,18 @@ import nlp_gensim_lib as nlpgen
 
 parameters = {
     'pos_or_neg': 'pos',
-
+    'sample_size': 5000,
     'num_topics': 3,
-    'passes': 5,
+    'passes': 10,
     #some possible values are 'auto' and 'symmetric'
     'alpha': 'symmetric',
     'print_num_topic': 3,
     'print_num_word': 3,
-    #some possible values are a scalar or None
-    'eta': None,
+    #some possible values are a scalar or None or 'auto'
+    'eta': 'auto',
 }
 
-reviews = mli.get_reviews_city("Phoenix", parameters.get('pos_or_neg'))[0:5000]
+reviews = mli.get_reviews_city("Phoenix", parameters.get('pos_or_neg'))[0:parameters.get('sample_size')]
 print('Number of reviews:', len(reviews))
 
 nlpgen.do_lda(reviews, parameters)
